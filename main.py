10082b4dae1f289f7bd28e55d1ae29b867b44f13
Pavlisio23 <fast.grom@gmail.com>
2025-05-03 19:08:36 +0300

    2 задача

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 @app.route('/index')
 def index():
-    title = request.args.get('title', 'Марсианская миссия')
+    title = "Марсианская миссия"
     return render_template('base.html', title=title)
+
+@app.route('/training/<prof>')
+def training(prof):
+    return render_template('training.html', prof=prof, title="Тренировки")
 
 if __name__ == '__main__':
     app.run(debug=True)
