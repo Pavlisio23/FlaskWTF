58d0b2550f2115452d851cc45dc959ad3967a7cd
Pavlisio23 <fast.grom@gmail.com>
2025-05-03 18:53:11 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/')
+@app.route('/index')
+def index():
+    title = request.args.get('title', 'Марсианская миссия')
+    return render_template('base.html', title=title)
+
+if __name__ == '__main__':
+    app.run(debug=True)
