e514d1daac301533afe6b653c800725f1039c7c3
Pavlisio23 <fast.grom@gmail.com>
2025-05-03 19:55:06 +0300

    3 задача

--- main.py
+++ main.py
@@ -12,5 +12,22 @@ def index():
 def training(prof):
     return render_template('training.html', prof=prof, title="Тренировки")
 
+@app.route('/list_prof/<list_type>')
+def list_prof(list_type):
+    professions = [
+        "Инженер-конструктор",
+        "Пилот",
+        "Строитель",
+        "Врач",
+        "Биолог",
+        "Геолог",
+        "Метеоролог",
+        "Астробиолог",
+        "Программист",
+        "Электротехник"
+    ]
+    return render_template('list_prof.html', list_type=list_type, professions=professions)
+
+
 if __name__ == '__main__':
     app.run(debug=True)
